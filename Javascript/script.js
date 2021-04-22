7a7d3421921cf8cc79845e4df3f16f9c4b59a0e9
sefaceren <h.sefaceren@gmail.com>
2021-04-22 05:17:17 +0300

    Javascript added.

---
+++ Javascript/script.js
@@ -0,0 +1,16 @@
+console.log("merhaba");
+console.log(1234);
+console.log(true);
+console.log([1,2,3]);
+console.error("hata oluştu");
+console.warn("bir uyarı oluştu");
+
+//tek satırlık yorum satırı
+
+/*
+yorum satırları
+yorum satırları
+*/
+
+
+console.clear();
